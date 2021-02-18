6058cc52b2f8a7598e9ee8047304b4fafcd64c3b
Anna Ossowska <ossowska.anna1@gmail.com>
2021-02-18 15:38:14 +0100

    Generic Lists under the hood

---
+++ 01_generics/GenericList/GenericList/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+
+// Showing how Generic Lists work under the hood
+
+namespace GenericList
+{
+    public class MyList<T>
+    {
+
+        T[] items = new T[5];
+        int count;
+
+        public void Add(T item)
+        {
+            if (count == items.Length)
+            {
+                Array.Resize(ref items, items.Length * 2);
+            }
+            else
+            {
+                items[count++] = item;
+            }
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var myList = new MyList<int>();
+            myList.Add(12);
+            myList.Add(11);
+            myList.Add(35);
+            myList.Add(10);
+            myList.Add(98);
+            myList.Add(102);
+        }
+    }
+}
