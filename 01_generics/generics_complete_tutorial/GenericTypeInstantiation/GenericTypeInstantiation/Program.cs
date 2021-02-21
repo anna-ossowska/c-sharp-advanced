6589d9065108e187a0b11a8d8243aa2f09bfab9c
Anna Ossowska <ossowska.anna1@gmail.com>
2021-02-21 17:58:34 +0100

    Generic type instantiation

---
+++ 01_generics/generics_complete_tutorial/GenericTypeInstantiation/GenericTypeInstantiation/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace GenericTypeInstantiation
+{
+    class MyClass<T>
+    {
+        public static int Value;
+
+        static MyClass()
+        {
+            Console.WriteLine(typeof(MyClass<T>));
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Value is not overwritten, because we get three different data types
+            MyClass<string>.Value = 10;
+            MyClass<int>.Value = 20;
+            MyClass<Program>.Value = 33;
+
+            Console.WriteLine(MyClass<string>.Value);
+        }
+    }
+}
