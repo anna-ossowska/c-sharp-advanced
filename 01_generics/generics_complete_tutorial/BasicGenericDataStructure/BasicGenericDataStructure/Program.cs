86f65daca1bc13b3dc2102f8c984d40c463e76b9
Anna Ossowska <ossowska.anna1@gmail.com>
2021-02-21 14:24:08 +0100

    Basic generic data structure

--- 01_generics/generics_complete_tutorial/BasicGenericDataStructure/BasicGenericDataStructure/Program.cs
+++ 01_generics/generics_complete_tutorial/BasicGenericDataStructure/BasicGenericDataStructure/Program.cs
@@ -4,13 +4,13 @@ using System.Collections.Generic;
 
 namespace BasicGenericDataStructure
 {
-    public class List
+    public class List<T>
     {
-        public int[] list = new int[3];
+        public T[] list = new T[3];
         public int index;
         public int Length { get { return list.Length; } }
 
-        public void Add(int item)
+        public void Add(T item)
         {
             if (index == list.Length)
             {
@@ -19,14 +19,14 @@ namespace BasicGenericDataStructure
             list[index++] = item;
         }
 
-        public int Get(int index)
+        public T Get(int index)
         {
             return list[index];
         }
 
         private void Grow()
         {
-            int[] temp = new int[list.Length * 2];
+            T[] temp = new T[list.Length * 2];
             Array.Copy(list, temp, list.Length);
             list = temp;
         }
@@ -37,15 +37,24 @@ namespace BasicGenericDataStructure
     {
         static void Main(string[] args)
         {
-            var myList = new List();
-            myList.Add(1);
-            myList.Add(2);
-            myList.Add(3);
-            myList.Add(4);
+            List<int> myListInt = new List<int>();
+            myListInt.Add(1);
+            myListInt.Add(2);
+            myListInt.Add(3);
+            myListInt.Add(4);
 
-            for (int i = 0; i < myList.Length; i++)
+            for (int i = 0; i < myListInt.Length; i++)
             {
-                Console.WriteLine(myList.Get(i));
+                Console.WriteLine(myListInt.Get(i));
+            }
+
+            List<string> myListString = new List<string>();
+            myListString.Add("Hello");
+            myListString.Add("World");
+
+            for (int i = 0; i < myListString.Length; i++)
+            {
+                Console.WriteLine(myListString.Get(i));
             }
         }
     }
