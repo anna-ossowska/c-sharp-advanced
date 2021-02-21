2d0ea634038f035c3efc992b6579032fa4cc4aa9
Anna Ossowska <ossowska.anna1@gmail.com>
2021-02-21 14:08:46 +0100

    Basic generic data structure

---
+++ 01_generics/generics_complete_tutorial/BasicGenericDataStructure/BasicGenericDataStructure/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace BasicGenericDataStructure
+{
+    public class List
+    {
+        public int[] list = new int[3];
+        public int index;
+        public int Length { get { return list.Length; } }
+
+        public void Add(int item)
+        {
+            if (index == list.Length)
+            {
+                Grow();
+            }
+            list[index++] = item;
+        }
+
+        public int Get(int index)
+        {
+            return list[index];
+        }
+
+        private void Grow()
+        {
+            int[] temp = new int[list.Length * 2];
+            Array.Copy(list, temp, list.Length);
+            list = temp;
+        }
+    }
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var myList = new List();
+            myList.Add(1);
+            myList.Add(2);
+            myList.Add(3);
+            myList.Add(4);
+
+            for (int i = 0; i < myList.Length; i++)
+            {
+                Console.WriteLine(myList.Get(i));
+            }
+        }
+    }
+}
