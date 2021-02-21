b1298d015cb34c02712f454953894f6ff5282aef
Anna Ossowska <ossowska.anna1@gmail.com>
2021-02-21 11:46:59 +0100

    Generics Intro

---
+++ 01_generics/generics_complete_tutorial/GenericsIntro/GenericsIntro/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GenericsIntro
+{
+    class Pair<T, U>
+    {
+        public T First { get; set; }
+        public U Second { get; set; }
+
+        public override string ToString()
+        {
+            return "{ " + First + ", " + Second + " }";
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Pair<int, int> p = new Pair<int, int> { First = 5, Second = 3 };
+            Pair<string, string> people1 = new Pair<string, string> { First = "Marry", Second = "John" };
+            Pair<string, string> people2 = new Pair<string, string> { First = "Samantha", Second = "Bob" };
+            Console.WriteLine(people1.ToString());
+        }
+
+    }
+}
