9e945af99f6809dc88458ea39b01d6fcc961957e
Anna Ossowska <ossowska.anna1@gmail.com>
2021-02-21 18:26:00 +0100

    Open and closed types

---
+++ 01_generics/generics_complete_tutorial/OpenClosedTypes/OpenClosedTypes/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace OpenClosedTypes
+{
+    // Open type is a type that takes a generic argument
+    class MyClass<T>
+    {
+        public static int Value;
+
+        static MyClass()
+        {
+            Console.WriteLine(typeof(MyClass<T>));
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Closed type, T is now a string
+            MyClass<string>.Value = 20;
+        }
+    }
+}
