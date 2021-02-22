564faf7d51e051f4d1cc21e564b07fb5f06401b0
Anna Ossowska <ossowska.anna1@gmail.com>
2021-02-22 19:27:18 +0100

    Generic constraints

---
+++ 01_generics/generics_complete_tutorial/GenericConstraintsInUse/GenericConstraintsInUse/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace GenericConstraintsInUse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            static void TakeA<T>(T arg) where T : IComparable
+            {
+            }
+
+            static void TakeB(IComparable arg)
+            {
+            }
+
+            // No boxing takes place if T is a value type
+            TakeA(5);
+
+            // Boxing takes place if T is a value type
+            TakeB(4);
+        }
+    }
+}
