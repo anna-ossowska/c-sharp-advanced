5a35d5b410a2504218f1e3a2334e5ff775b573fd
Anna Ossowska <ossowska.anna1@gmail.com>
2021-02-22 12:27:30 +0100

    Generic type inference

---
+++ 01_generics/generics_complete_tutorial/GenericTypeInference/GenericTypeInference/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace GenericTypeInference
+{
+    class RandomClass<T>
+    {
+        public RandomClass(T arg) { }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            static void P<T>(T item)
+            {
+                Console.WriteLine(item);
+            }
+
+            // Inference works with generic methods
+            // Same as: P<int>(5);
+            P(5);
+
+            // Inference does NOT work with generic classes
+            RandomClass<int> random = new RandomClass<int>(5);
+        }
+    }
+}
