ea71f22466193697aa57190f3bceaa314ca2148b
Anna Ossowska <ossowska.anna1@gmail.com>
2021-02-23 16:53:58 +0100

    Generics summary

---
+++ 01_generics/generics_complete_tutorial/Summary/Summary/Nullable.cs
@@ -0,0 +1,30 @@
+﻿namespace Summary
+{
+    // Nullable class is actually a part of .NET Framework, but I create it here for the learning purposes
+    public class Nullable<T> where T : struct
+    {
+        private object _value;
+
+        public Nullable()
+        {
+        }
+
+        public Nullable(T value)
+        {
+            _value = value;
+        }
+
+        public bool HasValue
+        {
+            get { return _value != null; }
+        }
+
+        public T GetValueOrDefault()
+        {
+            if (HasValue)
+                return (T)_value;
+
+            return default(T);
+        }
+    }
+}
---
+++ 01_generics/generics_complete_tutorial/Summary/Summary/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Summary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var number = new Nullable<int>();
+            Console.WriteLine("Has Value ?" + number.HasValue);
+            Console.WriteLine("Value: " + number.GetValueOrDefault());
+            
+
+            // Nullable class is actually a part of .NET Framework:
+            Nullable<int> i = new Nullable<int>(5);
+            Console.WriteLine(i.HasValue); // True
+
+            // Assigning value type to null
+            Nullable<int> j = new Nullable<int>();
+            Console.WriteLine(j.HasValue); // False
+
+            // Assigning value type to null (shortcut):
+            int? k = null;
+            Console.WriteLine(k.HasValue); // False
+        }
+    }
+}
