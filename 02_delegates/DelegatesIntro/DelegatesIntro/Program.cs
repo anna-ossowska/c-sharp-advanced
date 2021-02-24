751f7e032dad5c4437fd3a47ae7f2333319570c1
Anna Ossowska <ossowska.anna1@gmail.com>
2021-02-24 17:15:40 +0100

    Delegates intro

---
+++ 02_delegates/DelegatesIntro/DelegatesIntro/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace DelegatesIntro
+{
+    public delegate void MeDelegate();
+    // class MeDelagate { }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // same as: MeDelegate del = new MeDelegate(Foo);
+            MeDelegate del = Foo;
+
+            InvokeTheDelegate(Foo);
+        }
+
+        static void InvokeTheDelegate(MeDelegate deleg)
+        {
+            // same as: deleg.Invoke();
+            deleg();
+        }
+
+        static void Foo()
+        {
+            Console.WriteLine("Foo()");
+        }
+    }
+}
