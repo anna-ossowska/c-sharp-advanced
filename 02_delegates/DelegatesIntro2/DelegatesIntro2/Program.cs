989af9f089cea7762d4b1babd2a806355d0aba25
Anna Ossowska <ossowska.anna1@gmail.com>
2021-02-25 14:07:01 +0100

    Delegates intro

---
+++ 02_delegates/DelegatesIntro2/DelegatesIntro2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DelegatesIntro2
+{
+    class Program
+    {
+        delegate bool MeDelegate(int n);
+        public static bool GreaterThanThree(int n) { return n > 3; }
+        public static bool LessThanFive(int n) { return n < 5; }
+
+        static void Main(string[] args)
+        {
+            int[] numbers = new[] { 9, 1, 2, 3, 8, 4, 5 };
+            IEnumerable<int> result = RunNumbersThroughGauntlet(numbers, GreaterThanThree);
+            foreach (int n in result)
+            {
+                Console.WriteLine(n);
+            }
+        }
+
+        static IEnumerable<int> RunNumbersThroughGauntlet(IEnumerable<int> numbers, MeDelegate gauntlet)
+        {
+            foreach (int number in numbers)
+            {
+                if (gauntlet(number))
+                {
+                    yield return number;
+                }
+            }
+        }
+    }
+}
