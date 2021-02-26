a1dd74facf04bc039477f0de53f9ccadc540ed24
Anna Ossowska <ossowska.anna1@gmail.com>
2021-02-26 16:32:14 +0100

    Delegate Chains and Exceptions

---
+++ 02_delegates/DelegateChainsAndExceptions/DelegateChainsAndExceptions/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace DelegateChainsAndExceptions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Action deleg = (Action)Foo + MyException + Goo + Foo;
+            foreach (Action a in deleg.GetInvocationList())
+            {
+                try
+                {
+                    a();
+                }
+                catch
+                {
+                    Console.WriteLine("Exception");
+                }
+            }
+        }
+
+        static void Foo() { Console.WriteLine("Foo()"); }
+        static void MyException() { throw new Exception(); }
+        static void Goo() { Console.WriteLine("Goo()"); }
+    }
+}
