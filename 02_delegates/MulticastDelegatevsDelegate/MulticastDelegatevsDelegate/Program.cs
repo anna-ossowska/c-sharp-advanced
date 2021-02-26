dbb26d6915742b2efbd7db16d81f07a0ba4d32b2
Anna Ossowska <ossowska.anna1@gmail.com>
2021-02-26 11:41:19 +0100

    MulticastDelegate vs Delegate

---
+++ 02_delegates/MulticastDelegatevsDelegate/MulticastDelegatevsDelegate/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MulticastDelegatevsDelegate
+{
+    class Program
+    {
+        delegate int MeDelegate();
+
+        static void Main(string[] args)
+        {
+            // Inheritance chain:
+            // Delegate -> MulticastDelegate -> MeDelegate
+            MeDelegate d = ReturnFive;
+            d += ReturnTen;
+            d += ReturnTwelve;
+
+            foreach (int i in GetAllReturnValues(d))
+            {
+                Console.WriteLine(i);
+         
+            }
+        }
+
+        static List<int> GetAllReturnValues(MeDelegate d)
+        {
+            List<int> ints = new List<int>();
+
+            foreach (MeDelegate del in d.GetInvocationList())
+            {
+                ints.Add(del());
+            }
+            return ints;
+        }
+
+        static int ReturnFive() { return 5; }
+        static int ReturnTen() { return 10; }
+        static int ReturnTwelve() { return 12; }
+    }
+}
