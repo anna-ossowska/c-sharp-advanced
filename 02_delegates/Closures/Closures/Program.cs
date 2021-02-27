91062c3cee559a5bc4ecdb75ec7616c26885c4ad
Anna Ossowska <ossowska.anna1@gmail.com>
2021-02-27 10:18:28 +0100

    Closures

---
+++ 02_delegates/Closures/Closures/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Closures
+{
+    class Program
+    {
+        static void Main()
+        {
+            Action a = GiveMeAction();
+            a(); a(); a();
+        }
+
+        static Action GiveMeAction()
+        {
+            int i = 0;
+            return () => i++; // Closing/capturing the scope of i
+        }
+    }
+}
