56f95d643fe99247fee7c137a660e6ae476df24f
Anna Ossowska <ossowska.anna1@gmail.com>
2021-02-27 11:23:51 +0100

    Closures

---
+++ 02_delegates/Closures2/Closures2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Closures2
+{
+    class Program
+    {
+        static void Main()
+        {
+            Action a = GiveMeAction();
+            a();
+            a();
+            a();
+        }
+
+       static Action GiveMeAction()
+        {
+            Action ret = null;
+            int i = 0;
+            ret += () =>
+            {
+                Console.WriteLine("First method: " + i++);
+            };
+
+            ret += () =>
+            {
+                Console.WriteLine("Second method: " + i++);
+            };
+            return ret; // returning the chain of delegates
+        }
+    }
+}
