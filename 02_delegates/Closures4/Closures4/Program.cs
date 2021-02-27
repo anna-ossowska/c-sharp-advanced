b4bfa70fd010697fae3a1c4c82d1194fa02d66a8
Anna Ossowska <ossowska.anna1@gmail.com>
2021-02-27 16:11:05 +0100

    How the compiler implements Closures

---
+++ 02_delegates/Closures4/Closures4/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Closures4
+{
+    // How the compiler implements closures:
+    // C# compiler detects when a delegate forms a closure which is passed out of the current scope,
+    // and it promotes the delegate and the associated local variables into a compiler generated class.
+    // Each time we invoke the delegate we are actually calling the method on this class.
+    // Once we are no longer holding a reference to this delegate, the class can be garbage collected.
+
+    // Simple simulation:
+
+    class DisplayClass
+    {
+        public int i;
+        public void MethodGeneratedFromLambda()
+        {
+            i++;
+        }
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+            Action a = GiveMeAction();
+            Action b = GiveMeAction();
+            a(); // 1
+            a(); // 2
+            b(); // 1
+        }
+
+        static Action GiveMeAction()
+        {
+            return new Action(new DisplayClass().MethodGeneratedFromLambda);
+        }
+    }
+}
