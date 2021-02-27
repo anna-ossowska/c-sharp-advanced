194c80f25f58dc23724137b17055cf0f601bde27
Anna Ossowska <ossowska.anna1@gmail.com>
2021-02-27 11:38:38 +0100

    Closures

---
+++ 02_delegates/Closures3/Closures3/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Closures3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Func<int, int> x = GetFunc();
+            Console.WriteLine(x(5));
+            Console.WriteLine(x(6));
+        }
+
+        public static Func<int, int> GetFunc()
+        {
+            int i = 0;
+
+            Func<int, int> ret = (myValue) =>
+            {
+                i++;
+                return myValue + i;
+            };
+            return ret;
+        }
+    }
+}
