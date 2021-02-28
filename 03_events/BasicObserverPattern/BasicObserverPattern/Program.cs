d03bb54c10c31168b036b6f890592961a63e6175
Anna Ossowska <ossowska.anna1@gmail.com>
2021-02-28 18:50:30 +0100

    Basic Observer Pattern using delegates

---
+++ 03_events/BasicObserverPattern/BasicObserverPattern/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace BasicObserverPattern
+{
+    class TrainSignal
+    {
+        // delegate reference
+        public Action TrainIsComing;
+
+        public void HereComesATrain()
+        {
+            // some logic...
+            TrainIsComing();
+        }
+    }
+
+    class Car
+    {
+        public Car(TrainSignal trainSignal)
+        {
+            // We are observing the trainSignal
+            // When something happens, the trainSignal notifies us via the StopTheCar method
+            trainSignal.TrainIsComing += StopTheCar;
+        }
+
+        void StopTheCar()
+        {
+            Console.WriteLine("Screetch");
+        }
+
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+            TrainSignal trainSignal = new TrainSignal();
+            new Car(trainSignal);
+            new Car(trainSignal);
+            new Car(trainSignal);
+            trainSignal.HereComesATrain();
+        }
+    }
+}
