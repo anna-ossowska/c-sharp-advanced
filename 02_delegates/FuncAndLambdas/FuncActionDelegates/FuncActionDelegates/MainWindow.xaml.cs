40e4be3894333c8dbd25230f507a3fa45fca0b00
Anna Ossowska <ossowska.anna1@gmail.com>
2021-03-01 19:56:14 +0100

    Func and Lambdas

---
+++ 02_delegates/FuncAndLambdas/FuncActionDelegates/FuncActionDelegates/MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows;
+
+namespace FuncActionDelegates
+{
+    public partial class MainWindow : Window
+    {
+        // Declaring a variable of type PersonFormat which is a delegate type
+        // PersonFormat formatPerson;
+        Func<Person, string> formatPerson;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            PersonListBox.ItemsSource = People.GetPeople();
+        }
+
+        // In order to assign a value to formatPerson viariable we check the values of radio buttons inside the UI
+        // Then we assign an appropriate method
+        private void AssignDelegate()
+        {
+            if (Option1Button.IsChecked.Value)
+                formatPerson = p => p.ToString();
+
+            else if (Option2Button.IsChecked.Value)
+                formatPerson = p => p.LastName.ToUpper();
+ 
+            else if (Option3Button.IsChecked.Value)
+                formatPerson = p => p.FirstName.ToLower();
+
+            else if (Option4Button.IsChecked.Value)
+                formatPerson = p => string.Format("{0}, {1}", p.LastName, p.FirstName);
+        }
+
+        private void ProcessDataButton_Click(object sender, RoutedEventArgs e)
+        {
+            OutputList.Items.Clear();
+
+            AssignDelegate();
+
+            foreach (Person person in PersonListBox.Items)
+                OutputList.Items.Add(person.ToString(formatPerson));
+        }
+    }
+}
---
+++ 02_delegates/FuncAndLambdas/FuncActionDelegates/FuncActionDelegates/Snippets.cs
@@ -0,0 +1,57 @@
+﻿/* Formatters 
+
+        public static string Default(Person input)
+        {
+            return input.ToString();
+        }
+
+        public static string LastNameToUpper(Person input)
+        {
+            return input.LastName.ToUpper();
+        }
+
+        public static string FirstNameToLower(Person input)
+        {
+            return input.FirstName.ToLower();
+        }
+
+        public static string FullName(Person input)
+        {
+            return string.Format("{0}, {1}", input.LastName, input.FirstName);
+        }
+
+*/
+
+/* Assign Delegate
+
+            if (Option1Button.IsChecked.Value)
+                proc = Formatters.Default;
+            else if (Option2Button.IsChecked.Value)
+                proc = Formatters.LastNameToUpper;
+            else if (Option3Button.IsChecked.Value)
+                proc = Formatters.FirstNameToLower;
+            else if (Option4Button.IsChecked.Value)
+                proc = Formatters.FullName;
+
+*/
+
+/* Assign Action
+
+            if (OptionAButton.IsChecked.Value)
+                act += p => OutputList.Items.Add(
+                    p.Average(r => r.Rating).ToString("#.#"));
+
+            if (OptionBButton.IsChecked.Value)
+                act += p => OutputList.Items.Add(p.Min(s => s.StartDate));
+
+            if (OptionCButton.IsChecked.Value)
+                act += p => MessageBox.Show(
+                    p.OrderByDescending(r => r.Rating).First().LastName);
+
+            if (OptionDButton.IsChecked.Value)
+                act += p =>
+                    {
+                        p.ForEach(c => Console.Write(c.LastName[0]));
+                        Console.WriteLine();
+                    };
+*/
