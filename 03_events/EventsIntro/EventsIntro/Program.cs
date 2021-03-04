8cbe2c2aa3f04b99e6f901ec50af9ac17544cded
Anna Ossowska <ossowska.anna1@gmail.com>
2021-03-04 18:34:27 +0100

    Events Intro

---
+++ 03_events/EventsIntro/EventsIntro/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace EventsIntro
+{
+    class Newsletter
+    {
+        public event Action Subscribing;
+
+        public void ClickSubscribe()
+        {
+            Subscribing?.Invoke();
+            Console.WriteLine("You have subscribed to the newsletter.");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Newsletter n = new Newsletter();
+            n.Subscribing += () => Console.WriteLine("Clicking");
+            n.ClickSubscribe();
+        }
+    }
+}
