e7e893e241a984917a6f6ed5dbbed69d869d2545
Anna Ossowska <ossowska.anna1@gmail.com>
2021-03-05 12:25:43 +0100

    Events Intro

---
+++ 03_events/EventsIntro2/EventsIntro2/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace EventsIntro2
+{
+    public delegate void Notify(); // delegate
+
+    public class ProcessBusinessLogic // Publisher
+    {
+        public event Notify ProcessCompleted; // event
+
+        public void StartProcess()
+        {
+            Console.WriteLine("The process has started...");
+            OnProcessCompleted(); // raising an event
+        }
+
+        protected virtual void OnProcessCompleted()
+        {
+            // if ProcessCompleted is not null then call delegate
+            // this will call all the event handler methods registered with the ProcessCompleted event.
+            ProcessCompleted?.Invoke();
+        }
+
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+            ProcessBusinessLogic bl = new ProcessBusinessLogic();
+            bl.ProcessCompleted += bl_ProcessCompleted; // register with an event
+            bl.StartProcess();
+        }
+
+        // event handler
+        public static void bl_ProcessCompleted()
+        {
+            Console.WriteLine("Process completed!");
+        }
+    }
+}
