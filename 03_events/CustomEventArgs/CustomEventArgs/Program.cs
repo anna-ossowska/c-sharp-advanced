d6707b92d76ad3527abe66de1d06a9b69ef5fe21
Anna Ossowska <ossowska.anna1@gmail.com>
2021-03-05 19:11:44 +0100

    Passing Custom EventArgs

---
+++ 03_events/CustomEventArgs/CustomEventArgs/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace CustomEventArgs
+{
+    // If you want to pass more than one value as event data,
+    // create a class deriving from the EventArgs base class, as shown below.
+
+    public class ProcessEventArgs : EventArgs
+    {
+        public bool IsSuccessful { get; set; }
+        public DateTime CompletionTime { get; set; }
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+            ProcessBusinessLogic bl = new ProcessBusinessLogic();
+            bl.ProcessCompleted += bl_ProcessCompleted; // register with an event
+            bl.StartProcess();
+        }
+
+        // event handler
+        static void bl_ProcessCompleted(object sender, ProcessEventArgs e)
+        {
+            Console.WriteLine("Process " + (e.IsSuccessful ? "Completed Successfully" : "Failure"));
+            Console.WriteLine("Completion time: " + e.CompletionTime.ToLongDateString()); ;
+        }
+    }
+
+    public class ProcessBusinessLogic
+    {
+        // declaring an event using a bult-in EventHandler
+        public event EventHandler<ProcessEventArgs> ProcessCompleted;
+
+        public void StartProcess()
+        {
+            var data = new ProcessEventArgs();
+
+            try
+            {
+                Console.WriteLine("Process Started!");
+
+                data.IsSuccessful = true;
+                data.CompletionTime = DateTime.Now;
+                OnProcessCompleted(data); // passing event data
+            }
+            catch (Exception)
+            {
+
+                data.IsSuccessful = false;
+                data.CompletionTime = DateTime.Now;
+                OnProcessCompleted(data);
+            }
+        }
+        protected virtual void OnProcessCompleted(ProcessEventArgs e)
+        {
+            ProcessCompleted?.Invoke(this, e);
+        }
+    }
+}
