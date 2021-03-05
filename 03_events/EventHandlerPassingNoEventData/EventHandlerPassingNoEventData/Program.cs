25f4a03fa9ff37a03fadda4e0a4c6eb02651c2e2
Anna Ossowska <ossowska.anna1@gmail.com>
2021-03-05 17:32:32 +0100

    EventHandler, passing no event data

---
+++ 03_events/EventHandlerPassingNoEventData/EventHandlerPassingNoEventData/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace EventHandlerPassingNoEventData
+{
+    // Use the EventHandler delegate for all events that do not include event data
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ProcessBusinessLogic bl = new ProcessBusinessLogic();
+            bl.ProcessCompleted += bl_ProcessCompleted; // register with an event
+            bl.StartProcess();
+        }
+
+        // event handler
+        public static void bl_ProcessCompleted(object sender, EventArgs e)
+        {
+            Console.WriteLine("Process Completed!");
+        }
+    }
+
+    class ProcessBusinessLogic
+    {
+        // declaring an event using built-in EventHandler
+        public event EventHandler ProcessCompleted;
+
+        public void StartProcess()
+        {
+            Console.WriteLine("Process has started...");
+            OnProcessCompleted(EventArgs.Empty); // No event data
+        }
+
+        protected virtual void OnProcessCompleted(EventArgs e)
+        {
+            ProcessCompleted?.Invoke(this, e);
+        }
+    }
+}
