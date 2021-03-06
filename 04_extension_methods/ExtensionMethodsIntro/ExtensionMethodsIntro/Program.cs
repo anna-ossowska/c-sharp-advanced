4485e3c822102b43790b10cbb9e632404a0ed42f
Anna Ossowska <ossowska.anna1@gmail.com>
2021-03-06 13:52:57 +0100

    Extension methods - Intro

---
+++ 04_extension_methods/ExtensionMethodsIntro/ExtensionMethodsIntro/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ExtensionMethodsIntro
+{
+    // Extension method is a static method of a static class, where 'this' keyword is applied to the first parameter
+
+    static class Program
+    {
+        static DateTime Combine(this DateTime datePart, DateTime timePart)
+        {
+            return new DateTime(datePart.Year, datePart.Month, datePart.Day, timePart.Hour, timePart.Minute, timePart.Second);
+        }
+
+        static void Main(string[] args)
+        {
+            DateTime date = DateTime.Parse("1/5/2025");
+            DateTime time = DateTime.Parse("1/2/1998 9:55");
+
+            // Static method call
+            DateTime combined1 = Combine(date, time);
+
+            // Extension method call
+            DateTime combined2 = date.Combine(time);
+
+            Console.WriteLine(combined1);
+            Console.WriteLine(combined2);
+        }
+    }
+}
