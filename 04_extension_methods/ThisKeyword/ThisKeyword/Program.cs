a001a328a862c11f37724d5477ac71b6e28f03a3
Anna Ossowska <ossowska.anna1@gmail.com>
2021-03-06 20:04:00 +0100

    'this' keyword in extension methods

---
+++ 04_extension_methods/ThisKeyword/ThisKeyword/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace ThisKeyword
+{
+    // 'this' keyword and instance methods under the hood:
+    // In their roots instance methods are static
+
+    static class DataMethods
+    {
+        public static void Increment(this Num _this)
+        {
+            _this.numIncrement++;
+            Console.WriteLine("Incrementing: " + _this.numIncrement);
+        }
+    }
+
+    public class Num
+    {
+        public int numIncrement;
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+            Num n1 = new Num();
+            Num n2 = new Num();
+
+            n1.Increment();
+            n1.Increment();
+
+            n2.Increment();
+        }
+    }
+}
