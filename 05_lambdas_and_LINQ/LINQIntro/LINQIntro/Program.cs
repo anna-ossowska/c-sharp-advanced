b19324e579d1a817998aef9961d4b7059224ba35
Anna Ossowska <ossowska.anna1@gmail.com>
2021-03-08 12:23:29 +0100

    LINQ fluent and query syntax

---
+++ 05_lambdas_and_LINQ/LINQIntro/LINQIntro/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Linq;
+
+namespace LINQIntro
+{
+    class Program
+    {
+        static void Main()
+        {
+            int[] numbers = new[] { 2, 3, 4, 1, 6, 9, 5, 7 };
+
+            // Query syntax:
+            var result1 =
+                from n in numbers
+                where n < 5
+                select n;
+
+
+            // Fluent syntax:
+            var result2 =
+                numbers
+                .Where(n => n > 5)
+                .Select(n => n);
+
+
+            foreach (int i in result1)
+                Console.WriteLine(i);
+
+
+            foreach (int i in result2)
+                Console.WriteLine(i);
+        }
+    }
+}
