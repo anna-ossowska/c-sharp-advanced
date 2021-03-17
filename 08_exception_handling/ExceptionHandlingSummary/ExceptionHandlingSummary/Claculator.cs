75ef73c913ad1e5b422cfe0ddc7b611556d81e16
Anna Ossowska <ossowska.anna1@gmail.com>
2021-03-17 21:02:03 +0100

    Exception Handling - multiple catch blocks

---
+++ 08_exception_handling/ExceptionHandlingSummary/ExceptionHandlingSummary/Claculator.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExceptionHandlingSummary
+{
+    public class Calculator
+    {
+        public int Divide(int numerator, int denominator)
+        {
+            return numerator / denominator;
+        }
+    }
+}
---
+++ 08_exception_handling/ExceptionHandlingSummary/ExceptionHandlingSummary/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ExceptionHandlingSummary
+{
+    class Program
+    {
+        static void Main()
+        {
+            // Global Exception Handling block
+            try
+            {
+                var calculator = new Calculator();
+                calculator.Divide(5, 0);
+            }
+            // Exceptions go from more to less specific
+            catch (DivideByZeroException)
+            {
+                Console.WriteLine("You cannot divide by 0.");
+            }
+            catch (ArithmeticException)
+            {
+
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("A unexpected error.");
+            }
+        }
+    }
+}
