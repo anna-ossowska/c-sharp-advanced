e48cdc4061e7af93dd9d407a3b2b984342133399
Anna Ossowska <ossowska.anna1@gmail.com>
2021-03-17 21:51:06 +0100

    Example of 'using' statement inside 'try' block

---
+++ 08_exception_handling/UsingStatement/UsingStatement/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+
+namespace UsingStatement
+{
+    class Program
+    {
+        static void Main()
+        {
+            try
+            // With the 'using' statement compiler creates 'finally' block under the hood,
+            // which calls the Dispose() method on streamReader object
+            {
+                using (var streamReader = new StreamReader(@"c:\summary.zip"))
+                {
+                    var content = streamReader.ReadToEnd();
+                }
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Unexpected error occured.");
+            }
+        }
+    }
+}
