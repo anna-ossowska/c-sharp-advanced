9537454a88041ccef7be240b45433e82315e1a05
Anna Ossowska <ossowska.anna1@gmail.com>
2021-03-17 17:47:23 +0100

    Exception Handling

---
+++ 08_exception_handling/ExceptionHandling/ExceptionsDemo/ConsoleUI/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using ExceptionsLibrary;
+
+namespace ConsoleUI
+{
+    class Program
+    {
+        static void Main()
+        {
+            DemoCode demo = new DemoCode();
+
+            try
+            {
+                int result = demo.GrandParentMethod(4);
+                Console.WriteLine($"The value at the given position is: {result}");
+            }
+            catch (Exception ex)
+            {
+                // Using ex to get more information about the Exception itself
+                Console.WriteLine(ex.Message);
+                Console.WriteLine(ex.StackTrace);
+
+                var inner = ex.InnerException;
+
+                while (inner != null)
+                {
+                    Console.WriteLine(inner.StackTrace);
+                    inner = inner.InnerException;
+                }
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
+
---
+++ 08_exception_handling/ExceptionHandling/ExceptionsDemo/ExceptionsLibrary/DemoCode.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExceptionsLibrary
+{
+    public class DemoCode
+    {
+        public int GrandParentMethod(int position)
+        {
+            int output = 0;
+
+            Console.WriteLine("Open data base connection");
+
+            try
+            {
+                output = ParentMethod(position);
+            }
+            catch (Exception ex)
+            {
+                // throw vs throw ex:
+
+                // 'throw' is the proper way of putting the exception back into bubble up process.
+                // It gives the complete original StackTrace information.
+
+                // Throwing the exception object again ('throw ex')
+                // rewrites the Stack and gives us the wrong information.
+
+                // Creating a new exception without loosing the original StackTrace information
+                throw new ArgumentException("You passed in bad data", ex);
+            }
+            // This block will always be executed no matter whether an exception raised or not
+            finally
+            {
+                Console.WriteLine("Close data base connection.");
+            }
+
+            return output;
+        }
+
+        public int ParentMethod(int position)
+        {
+            return GetNumber(position);
+        }
+
+       public int GetNumber(int position)
+       {
+            int output = 0;
+
+            int[] numbers = new int[] { 1, 4, 7, 2 };
+            output = numbers[position];
+
+            return output;
+       }
+    }
+}
