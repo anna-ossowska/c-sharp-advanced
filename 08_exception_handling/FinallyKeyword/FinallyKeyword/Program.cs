6048bbede775a813392662ff1a68e442e12b8187
Anna Ossowska <ossowska.anna1@gmail.com>
2021-03-17 21:35:36 +0100

    Finally keyword and unmanaged resources

---
+++ 08_exception_handling/FinallyKeyword/FinallyKeyword/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+
+namespace FinallyKeyword
+{
+    class Program
+    {
+        static void Main()
+        {
+            StreamReader streamReader = null;
+            
+            try
+            {
+                streamReader = new StreamReader(@"c:\summary.zip");
+                var content = streamReader.ReadToEnd();
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Unexpected error");
+            }
+            // streamReader is not managed by CLR, so there is no Garbage Collection applied to it
+            // Performing the manual cleanup:
+            finally
+            {
+                if (streamReader != null)
+                {
+                    streamReader.Dispose();
+                } 
+            }
+        }
+    }
+}
