62ea2c9dd0aab19ac484b971ed065accb2feca76
Anna Ossowska <ossowska.anna1@gmail.com>
2021-03-18 15:05:34 +0100

    Delegates

---
+++ 02_delegates/Summary/Delegates/Program.cs
@@ -0,0 +1,34 @@
+﻿
+using System;
+
+namespace Delegates
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var processor = new PhotoProcessor();
+            var filters = new PhotoFilters();
+
+            // Multicast delegate holds the reference to more than one method
+            Action<Photo> filterHandler = filters.ApplyBrightness;
+            filterHandler += filters.ApplyContrast;
+            filterHandler += RemoveRedEyeFilter;
+
+            processor.Process("photo.jpg", filterHandler);
+
+            // Getting all the methods assigned to the delegate 
+            var delegMethods = filterHandler.GetInvocationList();
+
+            foreach (var item in delegMethods)
+            {
+                Console.WriteLine(item.Method);
+            }
+        }
+
+        static void RemoveRedEyeFilter(Photo photo)
+        {
+            Console.WriteLine("Apply RemoveRedEye");
+        }
+    }
+}
