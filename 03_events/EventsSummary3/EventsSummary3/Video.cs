c8d569c0928f2bfe09ce0f5b9e2856fa420348d0
Anna Ossowska <ossowska.anna1@gmail.com>
2021-03-19 22:33:58 +0100

    Events - summary

---
+++ 03_events/EventsSummary3/EventsSummary3/Video.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EventsSummary3
+{
+    public class Video
+    {
+        public string Title { get; set; }
+    }
+}
---
+++ 03_events/EventsSummary3/EventsSummary3/VideoEncoder.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+namespace EventsSummary3
+{
+    // Class containing the data about video that was encoded
+    public class VideoEventArgs : EventArgs
+    {
+        public Video Video { get; set; }
+    }
+
+    public class VideoEncoder
+    {
+        // 1. Define an event based on the EventHandler or EventHandler<TEventArgs> built-in delegate
+        // 2. Raise the event
+
+        public event EventHandler<VideoEventArgs> VideoEncoded;
+
+        public void Encode(Video video)
+        {
+            Console.WriteLine("Encoding...");
+            Thread.Sleep(3000);
+
+            // Notifying Subscribers
+            OnVideoEncoded(video);
+        }
+
+        virtual protected void OnVideoEncoded(Video video)
+        {
+            // Checking if there are any subscribers to the event
+            // Behind the scenes this is a list of pointers to methods
+            if (VideoEncoded != null)
+            {
+                VideoEncoded(this, new VideoEventArgs() { Video = video });
+            }
+        }
+    }
+}
+
