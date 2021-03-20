3d15ef8c1138d532b36cf813067d20150730d030
Anna Ossowska <ossowska.anna1@gmail.com>
2021-03-20 16:22:18 +0100

    Extension methods - Summary

---
+++ 04_extension_methods/ExtensionMethodsSummary/ExtensionMethodsSummary/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ExtensionMethodsSummary
+{
+    class Program
+    {
+        static void Main()
+        {
+            string post = "This is supposed to be a very long post.";
+
+            Console.WriteLine(post.Shorten(5));
+            Console.WriteLine(post.Shorten(2));
+        }
+    }
+}
---
+++ 04_extension_methods/ExtensionMethodsSummary/ExtensionMethodsSummary/StringExtensions.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace ExtensionMethodsSummary
+{
+    public static class StringExtension
+    {
+        public static string Shorten(this string str, int numberOfWords)
+        {
+            var words = str.Split(' ');
+
+            if (numberOfWords < 0)
+            {
+                throw new ArgumentOutOfRangeException("numberOfWords should be equal or greater than 0.");
+            }
+            else if (numberOfWords == 0)
+            {
+                return "";
+            }
+            else if (words.Length <= numberOfWords)
+            {
+                return str;
+            }
+            else
+            {
+                return string.Join(" ", words.Take(numberOfWords)) + " ...";
+            }
+        }
+    }
+}
